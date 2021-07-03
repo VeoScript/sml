486fa863b243e890304873d888f019cde0771bed
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-03 20:22:30 +0800

    style: change default color scheme

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -54,7 +54,7 @@ export default function Login({ all_users }) {
       <Head>
         <title>SML | Login</title>
       </Head>
-      <div className="flex items-center justify-center w-full h-screen overflow-y-auto bg-sml-dark text-sml-white">
+      <div className="flex items-center justify-center w-full h-screen overflow-y-auto bg-ghostwhite text-panther">
         <Toaster
           position="top-center"
           reverseOrder={true}
@@ -64,29 +64,29 @@ export default function Login({ all_users }) {
             <div className="flex flex-col space-y-2">
               <div className="flex flex-col">
                 <h1 className="font-normal text-2xl">VEOSCRIPT</h1>
-                <span className="font-light text-xl text-sml-mocha">Presents</span>
+                <span className="font-light text-xl">Presents</span>
               </div>
-              <h1 className="font-bold text-5xl text-sml-mocha">#ShareMoLang</h1>
-              <h2 className="font-normal text-xl text-gray-300">i-share mo lang sa amin lahat ng ka gagohan mo.</h2>
+              <h1 className="font-bold text-5xl text-marigold">#ShareMoLang</h1>
+              <h2 className="font-normal text-xl text-cool-gray">Share mo lang dito lahat ng mga kagaguhan mo.</h2>
             </div>
           </div>
-          <div className="flex flex-col items-center w-full space-y-3">
+          <div className="flex flex-col items-center w-full space-y-4">
             <form onSubmit={handleSubmit(handleLogin)} className="flex flex-col w-full max-w-sm space-y-2">
               <h1 className="font-semibold text-xl ml-2 mb-2">Login</h1>
               <div className="flex flex-col w-full space-y-1">
-                <input className="w-full px-5 py-3 rounded-lg bg-sml-dim text-sml-white focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" type="text" name="username" placeholder="Username" {...register("username", { required: true })} disabled={isSubmitting} />
-                {errors.username && <span className="text-xs text-sml-mocha ml-1">Username is required.</span>}
+                <input className="w-full px-5 py-3 rounded-lg bg-white border border-cool-gray focus:border-marigold focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" type="text" name="username" placeholder="Username" {...register("username", { required: true })} disabled={isSubmitting} />
+                {errors.username && <span className="text-xs ml-1">Username is required.</span>}
               </div>
               <div className="flex flex-col w-full space-y-1">
-                <input className="w-full px-5 py-3 rounded-lg bg-sml-dim text-sml-white focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" type="password" name="password" placeholder="Password" {...register("password", { required: true })} disabled={isSubmitting} />
-                {errors.password && <span className="text-xs text-sml-mocha ml-1">Password is required.</span>}
+                <input className="w-full px-5 py-3 rounded-lg bg-white border border-cool-gray focus:border-marigold focus:outline-none disabled:cursor-not-allowed disabled:opacity-50" type="password" name="password" placeholder="Password" {...register("password", { required: true })} disabled={isSubmitting} />
+                {errors.password && <span className="text-xs ml-1">Password is required.</span>}
               </div>
-              <button className="w-full px-5 py-3 rounded-lg bg-sml-dim text-sml-mocha transition ease-in-out duration-200 focus:outline-none hover:opacity-80 disabled:cursor-not-allowed disabled:opacity-50" type="submit" disabled={isSubmitting}>Login</button>
+              <button className="w-full px-5 py-3 rounded-lg bg-marigold text-white transition ease-in-out duration-200 focus:outline-none hover:opacity-80 disabled:cursor-not-allowed disabled:opacity-50" type="submit" disabled={isSubmitting}>Login</button>
             </form>
-            <div className="flex flex-col items-center w-full max-w-sm space-y-2">
-              <hr className="w-full border border-sml-coffee opacity-20" />
+            <div className="flex flex-col items-center w-full max-w-sm space-y-4">
+              <hr className="w-full border border-panther opacity-10" />
               <Link href="/signup">
-                <a className="w-full px-5 py-3 rounded-lg bg-sml-mocha text-sml-dim text-center transition ease-in-out duration-200 focus:outline-none hover:opacity-80">Create Account</a>
+                <a className="w-full px-5 py-3 rounded-lg bg-panther text-white text-center transition ease-in-out duration-200 focus:outline-none hover:opacity-80">Create Account</a>
               </Link>
             </div>
           </div>
