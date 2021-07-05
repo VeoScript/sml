62820b5cffd7ac83f8ad9b8a3100e340a822d7db
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-05 14:59:11 +0800

    feat: add post author credentials

--- src/pages/api/posts/create.js
+++ src/pages/api/posts/create.js
@@ -6,7 +6,11 @@ export default async (req, res) => {
   const post = await prisma.post.create({
     data: {
       content: req.body.create_post,
-      authorId: req.body.authorId
+      authorId: req.body.authorId,
+      authorImage: req.body.authorImage,
+      authorName: req.body.authorName,
+      authorUsername: req.body.authorUsername,
+      createdAt: req.body.createdAt
     }
   })
   res.json(post)
