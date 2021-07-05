c34ab65a80668434ce920d9f90b4768cf29a1899
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-05 16:59:43 +0800

    chore: remove onKeyPress() (#24)

--- src/components/ContentEditable.jsx
+++ src/components/ContentEditable.jsx
@@ -64,7 +64,7 @@ export default function ContentEditable({ account }) {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                 </svg>
               </button>
-              <button type="submit" onKeyPress={(e) => e.ke && handleSubmit(handleCreatePost)} disabled={isSubmitting}>
+              <button type="submit" disabled={isSubmitting}>
                 <svg className="w-5 h-5 text-gray-500 hover:text-marigold transition ease-in-out duration-300 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <path d="M0 12l11 3.1 7-8.1-8.156 5.672-4.312-1.202 15.362-7.68-3.974 14.57-3.75-3.339-2.17 2.925v-.769l-2-.56v7.383l4.473-6.031 4.527 4.031 6-22z"/>
                 </svg>
