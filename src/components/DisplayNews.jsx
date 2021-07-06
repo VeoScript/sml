4d26cb33748df7bffe53878a1ded721baaddb8e8
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-06 08:36:11 +0800

    chore: update display-news component (#29)

--- src/components/DisplayNews.jsx
+++ src/components/DisplayNews.jsx
@@ -4,11 +4,11 @@ export default function DisplayNews({ news }) {
   return (
     <div className="flex flex-col w-full h-full overflow-y-auto px-3 pb-3 border-b border-marigold border-opacity-30 space-y-3">
       <h1 className="font-bold text-base text-gray-500 mb-1">Pilipinas Balita</h1>
-      {news.articles.map((({ source, author, title, url, urlToImage, publishedAt, content }, i) => {
+      {news.articles.map((({ author, title, url, urlToImage }, i) => {
         return (
-          <Link href={url}>
-            <a target="_blank" className="flex flex-row items-start justify-between w-full" key={i}>
-              <div className="flex flex-row items-center space-x-2">
+          <Link href={url} key={i}>
+            <a target="_blank" className="flex flex-row items-start justify-between w-full">
+              <div className="flex flex-row items-start space-x-2">
                 <img className="w-full max-w-[3rem] h-10 rounded-sm object-cover" src={ !urlToImage ? 'https://blog.remitly.com/wp-content/uploads/2018/06/philippines-1195394_1920.jpg' : urlToImage } />
                 <div className="flex flex-col">
                   <span className="font-bold text-sm">{author}</span>
