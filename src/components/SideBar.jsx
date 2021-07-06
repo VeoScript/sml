895501ad4a5f54569801150c7bdb66d4f317c427
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-07-06 22:48:31 +0800

    style: change sidebar button styles

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -22,28 +22,28 @@ export default function SideBar({ account }) {
         <img className="w-20 h-20 rounded-full object-cover bg-marigold" src={account.image} />
         <div className="flex flex-col w-full">
           <span className="font-bold text-2xl text-panther">{account.name}</span>
-          <span className="font-light text-base text-panther">@{account.username}</span>
+          <span className="font-light text-base text-panther">{account.username}</span>
         </div>
         <div className="flex flex-col w-full py-5 space-y-1">
           {navigations.map(({ name, icon, href }, i) => (
             <Link href={href} key={i}>
-              <a className={`${router.pathname === href ? 'bg-marigold text-white hover:bg-opacity-80' : 'bg-none hover:bg-marigold hover:bg-opacity-30'} flex flex-row items-center text-lg text-panther rounded-full space-x-3 px-5 py-3 transition ease-in-out duration-300`}>
+              <a className={`${router.pathname === href ? 'bg-marigold text-white hover:bg-opacity-80' : ' text-opacity-80 bg-none hover:bg-marigold hover:bg-opacity-30'} flex flex-row items-center text-lg text-panther rounded-full space-x-3 px-5 py-2 transition ease-in-out duration-300`}>
                 <span>{icon}</span>
                 <span>{name}</span>
               </a>
             </Link>
           ))}
-          <button onClick={handleLogout} className="flex flex-row items-center text-lg text-panther rounded-full space-x-3 px-5 py-3 transition ease-in-out duration-300 bg-none hover:bg-marigold hover:bg-opacity-30">
+          <button onClick={handleLogout} className="flex flex-row items-center text-lg text-panther text-opacity-80 rounded-full space-x-3 px-5 py-2 transition ease-in-out duration-300 bg-none hover:bg-marigold hover:bg-opacity-30">
             <svg className="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M14 19v-.083c-1.178.685-2.542 1.083-4 1.083-4.411 0-8-3.589-8-8s3.589-8 8-8c1.458 0 2.822.398 4 1.083v-2.245c-1.226-.536-2.576-.838-4-.838-5.522 0-10 4.477-10 10s4.478 10 10 10c1.424 0 2.774-.302 4-.838v-2.162zm4-9.592l2.963 2.592-2.963 2.592v-1.592h-8v-2h8v-1.592zm-2-4.408v4h-8v6h8v4l8-7-8-7z"/></svg>
             <span>Logout</span>
           </button>
         </div>
       </div>
-      <div className="flex flex-col items-center w-full space-y-3">
-        <button className="w-full text-base text-white bg-marigold rounded-full space-x-3 px-5 py-3 transition ease-in-out duration-300 hover:bg-opacity-80">
+      <div className="flex flex-col items-center w-full space-y-5">
+        <button className="w-full text-base text-white bg-marigold rounded-full space-x-3 px-5 py-2 mb-5 transition ease-in-out duration-300 hover:bg-opacity-80">
           <span>Write Post</span>
         </button>
-        <span className="text-xs opacity-50">&copy;&nbsp;{ new Date().getFullYear() } SML #ShareMoLang, Veoscript.</span>
+        <span className="text-[11px] opacity-80">&copy;&nbsp;{ new Date().getFullYear() } SML #sharemolang, Veoscript.</span>
       </div>
     </div>
   )
